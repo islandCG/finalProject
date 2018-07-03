84f28f3e27b915f6d262081f6f4462606e82f7e3
coder-kk <zhangwankang807522215@gmail.com>
2018-07-04 00:10:17 +0800

    注释掉了粒子系统部分

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -8,23 +8,134 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include<sstream>
-
 #include "shader_m.h"
 #include "camera.h""
 #include <vector>
 #include "model.h"
-
 #include <iostream>
-
 #include <freeglut/freeglut.h>
-
 #include <gltools/GLTools.h>
 #include "Particle.h"
 using namespace std;
 
 /** 创建一个粒子类对象 */
-CParticle Snow;
+//CParticle Snow;
+///** 用来设置粒子的属性值 */
+//float x, y, z, vx, vy, vz, ax, ay, az, size, lifetime, dec;
+//int r, g, b;
 
+/** 初始化雪花粒子 */
+//bool InitSnow()
+//{
+//	for (int i = 0; i < Snow.GetNumOfParticle(); ++i)
+//	{
+//		///初始化颜色（白色）
+//		r = 255;
+//		g = 255;
+//		b = 255;
+//		Snow.SetColor(i, r, g, b);
+//
+//		///初始化坐标
+//		x = 0.1f * (rand() % 50) - 2.5f;
+//		y = 2 + 0.1f * (rand() % 2);
+//		if ((int)x % 2 == 0)
+//			z = rand() % 6;
+//		else
+//			x = -rand() % 3;
+//		Snow.SetPosition(i, x, y, z);
+//
+//		///初始化速度
+//		vx = 0.00001 * (rand() % 100);
+//		vy = 0.0000002 * (rand() % 28000);
+//		vz = 0;
+//		Snow.SetVelocity(i, vx, vy, vz);
+//
+//		///初始化加速度
+//		ax = 0;
+//		ay = 0.000005f;
+//		az = 0;
+//		Snow.SetAcceleration(i, ax, ay, az);
+//
+//		///初始化生命周期
+//		lifetime = 100;
+//		Snow.SetLifeTime(i, lifetime);
+//
+//		///消失速度
+//		dec = 0.005 * (rand() % 50);
+//		Snow.SetDec(i, dec);
+//
+//		///初始化大小
+//		Snow.SetSize(i, 0.03f);
+//	}
+//	return true;
+//}
+/** 更新粒子 */
+//void UpdateSnow()
+//{
+//	/** 更新位置 */
+//	x += (vx * 5);
+//	y -= vy;
+//
+//	/** 更新速度 */
+//	vy += ay;
+//
+//	/** 更新生存时间 */
+//	lifetime -= dec;
+//
+//	if (x > 3)
+//		x = -2;
+//
+//	/** 如果粒子消失或生命结束 */
+//	if (y <= -1 || lifetime <= 0)
+//	{
+//		/** 初始化位置 */
+//		x = 0.1f * (rand() % 50) - 2.5f;
+//		y = 2 + 0.1f * (rand() % 2);
+//		if ((int)x % 2 == 0)
+//			z = rand() % 6;
+//		else
+//			z = -rand() % 3;
+//
+//		/** 初始化速度 */
+//		vx = (float)(0.00001 * (rand() % 100));
+//		vy = (float)(0.0000002 * (rand() % 28000));
+//		vz = 0;
+//
+//		/** 初始化加速度 */
+//		ax = 0;
+//		ay = 0.000005f;
+//		az = 0;
+//		lifetime = 100;
+//		dec = 0.005*(rand() % 50);
+//	}
+//}
+//void DrawParticle()
+//{
+//	/** 绑定纹理 */
+//	glBindTexture(GL_TEXTURE_2D, texName[1]);
+//
+//	for (int i = 0; i<Snow.GetNumOfParticle(); ++i)
+//	{
+//		/** 获得粒子的所有属性 */
+//		Snow.GetAll(i, r, g, b, x, y, z, vx, vy, vz, ax, ay, az, size, lifetime, dec);
+//		glLoadIdentity();
+//		glTranslatef(0.0f, 0.0f, -6.0f);
+//		glColor4ub(r, g, b, 255);
+//		glNormal3f(0.0f, 0.0f, 1.0f);   /**< 定义法线方向 */
+//										/** 画出粒子 */
+//		glBegin(GL_QUADS);
+//		glTexCoord2f(0.0f, 0.0f); glVertex3f(x - size, y - size, z);
+//		glTexCoord2f(1.0f, 0.0f); glVertex3f(x - size, y + size, z);
+//		glTexCoord2f(1.0f, 1.0f); glVertex3f(x + size, y + size, z);
+//		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + size, y - size, z);
+//		glEnd();
+//
+//		/** 更新粒子属性 */
+//		UpdateSnow();
+//		Snow.SetAll(i, r, g, b, x, y, z, vx, vy, vz, ax, ay, az, size, lifetime, dec);
+//	}
+//	glutPostRedisplay();//发送渲染请求
+//}
 /// Holds all state information relevant to a character as loaded using FreeType
 struct Character {
 	GLuint TextureID;   // ID handle of the glyph texture
